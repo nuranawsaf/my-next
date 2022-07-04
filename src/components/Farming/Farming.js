97ead658bccf39a92fd92868baa7dbb860ccc523
nuranawsaf <nuranawsaf712@gmail.com>
2022-07-04 13:36:55 +0600

    last

--- src/components/Farming/Farming.js
+++ src/components/Farming/Farming.js
@@ -2,17 +2,17 @@ import React from 'react'
 
 function Farming() {
   return (
-    <div className='container mt-[200px] gap-8 columns-2'>
+    <div className='container mt-[300px] lg:mt-[200px] lg:gap-8 grid lg:grid-cols-2'>
         <div>
             <img src="/Rectangle 8.png" alt=""/>
         </div>
-        <div>
-            <h1>Farming First is SVAD’s way</h1>
+        <div className=''>
+            <h1 className='text-5xl font-bold w-[482px] lg:w-auto text-center mt-[40px] lg:mt-auto lg:text-left '>Farming First is SVAD’s way</h1>
             <br/>
-            <p className='text-gray text-xl'>We are here to serve our communities by offering</p>
+            <p className='text-gray text-xl text-center'>We are here to serve our communities by offering</p>
             <br/>
             <br/>
-            <h6 className='text-gray text-xl mb-4 flex '>
+            <h6 className='text-gray text-xl mb-4 flex text-center'>
               <img src="/Group.png" alt=""/>
              <span className='ml-3'>the freshest products</span>
             </h6>
--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -4,32 +4,32 @@ function Header() {
   return (
     <section className='relative   header  h-[864px] mb-8 flex items-center justify-center'>
        <div className=' text-white '>
-            <h3 className='text-4xl'>SVAD FARM</h3>
-            <h1 className='text-8xl'>The Best Price & <br/> Freshest Always.</h1>
+            <h3 className='txt-xl md:text-2xl lg:text-4xl'>SVAD FARM</h3>
+            <h1 className='text-3xl md:text-6xl lg:text-8xl'>The Best Price & <br/> Freshest Always.</h1>
         </div>
 
-    <div className=' absolute  left-1/2 -translate-x-1/2 bottom-[0px] translate-y-1/2 mt-6 flex items-end justify-center gap-x-4'>
-        <div class="bg-white dark:bg-slate-800 w-[330px] pt-5 pb-5  px-6 py-8 shadow-xl flex items-center justify-center p-2 bg-indigo-500  shadow-lg">
+    <div className='sb absolute  left-1/2 -translate-x-1/2 bottom-[0px] translate-y-1/2  lg:mt-6 flex flex-col lg:flex-row gap-y-2 lg:gap-y-0  lg:gap-x-4'>
+        <div className="bg-white dark:bg-slate-800 w-[330px] pt-5 pb-5  px-6 py-8 shadow-xl flex items-center justify-center p-2 bg-indigo-500  ">
             <img src="/icon.png" alt=""/>
             <div className='ml-4'>
-                <h3 class="  font-medium ">Lowest Price</h3>
-                <p class="text-gray text-slate-500 dark:text-slate-400 text-sm">We will match or <br/> beat any price</p>
+                <h3 className="  font-medium ">Lowest Price</h3>
+                <p className="text-gray text-slate-500 dark:text-slate-400 text-sm">We will match or <br/> beat any price</p>
             </div>
         </div>
 
-        <div class="bg-white dark:bg-slate-800 w-[330px] pt-5 pb-5  px-6 py-8 shadow-xl flex items-center justify-center p-2 bg-indigo-500  shadow-lg">
+        <div className="bg-white dark:bg-slate-800 w-[330px] pt-5 pb-5  px-6 py-8 shadow-xl flex items-center justify-center p-2 bg-indigo-500  ">
         <img src="/icon.png" alt=""/>
         <div className='ml-4'>
-            <h3 class="  font-medium ">Lowest Price</h3>
-            <p class="text-gray text-slate-500 dark:text-slate-400 text-sm">We will match or <br/> beat any price</p>
+            <h3 className="  font-medium ">Lowest Price</h3>
+            <p className="text-gray text-slate-500 dark:text-slate-400 text-sm">We will match or <br/> beat any price</p>
         </div>
         </div>
 
-        <div class="bg-white dark:bg-slate-800 w-[330px] pt-5 pb-5  px-6 py-8 shadow-xl flex items-center justify-center p-2 bg-indigo-500  shadow-lg">
+        <div className="bg-white dark:bg-slate-800 w-[330px] pt-5 pb-5  px-6 py-8 shadow-xl flex items-center justify-center p-2 bg-indigo-500  ">
             <img src="/icon.png" alt=""/>
             <div className='ml-4'>
-                <h3 class="  font-medium ">Lowest Price</h3>
-                <p class="text-gray text-slate-500 dark:text-slate-400 text-sm">We will match or <br/> beat any price</p>
+                <h3 className="  font-medium ">Lowest Price</h3>
+                <p className="text-gray text-slate-500 dark:text-slate-400 text-sm">We will match or <br/> beat any price</p>
             </div>
         </div>
     </div>
